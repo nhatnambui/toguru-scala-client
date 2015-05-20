fe5f1a853d0123e9eb093b57858f0e36750163ea
Chris Wewerka <breadfan@gmx.de>
2015-05-20 08:32:10 +0200

    #324: json parsing code cleanup

--- src/main/scala/featurebee/json/FeatureJsonProtocol.scala
+++ src/main/scala/featurebee/json/FeatureJsonProtocol.scala
@@ -14,49 +14,53 @@ object FeatureJsonProtocol extends DefaultJsonProtocol {
 
   implicit object StateJsonFormat extends RootJsonFormat[StateType] {
     val stateEnums = State.values.toList
-    override def write(obj: StateType): JsValue = ???
+    override def write(obj: StateType): JsValue = throw new DeserializationException("Write not supported for state")
     override def read(json: JsValue): StateType = {
-      stateEnums.find{
-        st => val state = st.toString.toLowerCase
-          val jsonState: JsString = json.asInstanceOf[JsString]
-          state.toLowerCase == jsonState.value.toLowerCase
+      stateEnums.find {
+        st =>
+          json match {
+            case jsStringState: JsString =>
+              val state = st.toString.toLowerCase
+              state.toLowerCase == jsStringState.value.toLowerCase
+            case _ => throw new DeserializationException(s"State has to be a json string: $json")
+          }
       }.getOrElse(throw new DeserializationException(s"Unknown state ${json.toString()}"))
     }
   }
 
   implicit object ConditionJsonFormat extends RootJsonFormat[Condition] {
+
+    val allBrowsers = Browser.values.toList
     
     def mapLocales(locales: Vector[JsValue]) = {
       val jLocales = locales.toList.map {
-        e =>
-          val splitted = e.asInstanceOf[JsString].value.split('-')
-          splitted.size match {
+        case jsString: JsString =>
+          val splitted = jsString.value.split('-')
+          splitted.length match {
             case 2 => new Locale(splitted(0), splitted(1))
             case 1 => new Locale(splitted(0))
-            case _ => throw new Exception("invalid locale string in feature description")
+            case _ => throw new DeserializationException(s"Invalid locale string in feature description: ${jsString.value}")
           }
-
-
+        case other => throw new DeserializationException(s"Culture should be a json string but is ${other.getClass}")
       }
       CultureCondition(jLocales.toSet)
     }
 
     def mapBrowsers(browsers: Vector[JsValue]) = {
       val brs = browsers.toList.map {
-        e =>
-          val b = e.asInstanceOf[JsString].value
-          val allBrowsers = Browser.values.toList
-          allBrowsers.find(br => br.toString.toLowerCase == b.toLowerCase).getOrElse(Browser.Other)
+        case jsStringBrowser: JsString =>
+          allBrowsers.find(br => br.toString.toLowerCase == jsStringBrowser.value.toLowerCase).getOrElse(Browser.Other)
+        case other => throw new DeserializationException(s"Browser should be a json string but is ${other.getClass}")
       }
       BrowserCondition(brs.toSet)
     }
     
-    def write(c: Condition) = ???
+    def write(c: Condition) = throw new DeserializationException("Write not supported for conditions")
     def read(value: JsValue) = {
       value.asJsObject.fields.get("culture").collect { case JsArray(locales) => mapLocales(locales) }.orElse {
         value.asJsObject.fields.get("browser").collect { case JsArray(browsers) => mapBrowsers(browsers)}
         // TODO traffic dist
-      }.getOrElse(throw new DeserializationException("Subclass of Condition expected"))
+      }.getOrElse(throw new DeserializationException(s"Unsupported condition(s): ${value.asJsObject.fields.keys}"))
     }
   }
 
