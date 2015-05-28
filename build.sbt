87c33dfd4ecec6f6558ee1f66772aab00dd31b74
Chris Wewerka <breadfan@gmx.de>
2015-05-28 15:28:24 +0200

    added play support for ReqHeader -> ClientInfo

--- build.sbt
+++ build.sbt
@@ -8,10 +8,11 @@ version in ThisBuild := "1.0." + Properties.envOrElse("TRAVIS_BUILD_NUMBER", "0-
 
 scalaVersion := "2.11.6"
 
-// Change this to another test framework if you prefer
 libraryDependencies ++= Seq(
   "io.spray" %%  "spray-json" % "1.3.2",
   "commons-io" % "commons-io" % "2.4",
+  "com.typesafe.play" %% "play" % "2.3.9" % "optional",
+  "com.typesafe.play" %% "play-test" % "2.3.9" % "optional",
   "org.scalatest" %% "scalatest" % "2.2.4" % "test",
   "org.mockito" % "mockito-core" % "2.0.8-beta" % "test"
 )
---
+++ src/main/scala/featurebee/play/PlayClientInfoSupport.scala
@@ -0,0 +1,68 @@
+package featurebee.play
+
+import java.util.{UUID, Locale}
+
+import play.api.http.HeaderNames
+import play.api.i18n.Lang
+import play.api.mvc.RequestHeader
+import featurebee.FeaturesString._
+import featurebee.api.Feature._
+
+/**
+ * Default support methods for converting a [[RequestHeader]] to a [[featurebee.ClientInfo]].
+ *
+ * If your fine with the way this object extracts the locale, UUID, and forced feature activation string from the play
+ * request header you simply have to write s.th like this in your app, and import it via import PlayControllerSupport._ in
+ * your Controller classes.
+ *
+ * {{{
+ *   object PlayControllerSupport {
+
+    implicit def requestToClientInfo(implicit requestHeader: RequestHeader): ClientInfo = {
+      import PlayClientInfoSupport._
+      ClientInfoImpl(userAgent, localeFromCookieValue("culture"), uuidFromCookieValue("as24Visitor"), forcedFeatureToggle)
+    }
+  }
+ * }}}
+ */
+object PlayClientInfoSupport {
+
+  def userAgent(implicit requestHeader: RequestHeader) = requestHeader.headers.get(HeaderNames.USER_AGENT)
+
+  def localeFromCookieValue(cookieName: String)(implicit requestHeader: RequestHeader): Option[Locale] = for {
+    cultureCookie <- requestHeader.cookies.get(cookieName)
+    lang <- Lang.get(cultureCookie.value)
+  } yield lang.toLocale
+
+  def uuidFromCookieValue(cookieName: String)(implicit requestHeader: RequestHeader): Option[UUID] =
+    requestHeader.cookies.get(cookieName).map(c => UUID.fromString(c.value))
+
+  def forcedFeatureToggle(featureName: String)(implicit requestHeader: RequestHeader): Option[Boolean] = {
+
+    def lowerCaseKeys[T](m: Map[String,T]) = m.map { case (k, v) => (k.toLowerCase, v) }
+
+    val reqHeaders = lowerCaseKeys(requestHeader.headers.toSimpleMap)
+    lazy val maybeForcedFromHeader = reqHeaders.get("x-featurebee").orElse(reqHeaders.get("featurebee")).
+      flatMap(featuresString => parseForcedFeaturesString(featuresString)(featureName))
+    lazy val maybeForcedFromCookie = requestHeader.cookies.get("featurebee").flatMap(cookie => parseForcedFeaturesString(cookie.value)(featureName))
+
+    lazy val lowerCasedKeysQueryStringMap = lowerCaseKeys(requestHeader.queryString)
+
+    lazy val parseFeatureQueryString: FeatureName => Option[Boolean] = {
+      val maybeFeatureBeeString: Option[List[String]] = lowerCasedKeysQueryStringMap.get("featurebee").map(_.toList)
+
+      featureName => {
+        maybeFeatureBeeString.map {
+          case Nil => None
+          case featureString :: Nil =>
+            parseForcedFeaturesString(featureString)(featureName)
+          case other => throw new Exception(s"featurebee string has more than one element: $other ${other.size}")
+        }.getOrElse(None)
+      }
+    }
+
+    val maybeForcedFromQueryParam = parseFeatureQueryString(featureName)
+    maybeForcedFromQueryParam.orElse(maybeForcedFromHeader).orElse(maybeForcedFromCookie)
+  }
+}
+
