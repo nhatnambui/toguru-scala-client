442ad9532ec00909615c6f18d1d8617c72d6e019
Chris Wewerka <breadfan@gmx.de>
2016-05-24 09:53:44 +0200

    made it a singleton

--- s3-registry/src/main/scala/featurebee/registry/DefaultFeatureValueFeatureRegistry.scala
+++ s3-registry/src/main/scala/featurebee/registry/DefaultFeatureValueFeatureRegistry.scala
@@ -7,7 +7,7 @@ import featurebee.api.{Feature, FeatureRegistry}
   * Always returns None for a requested feature. This is useful to always default to the hardcoded value of the feature as a fallback for registries that
   * may reload from remotes locations
   */
-class DefaultFeatureValueFeatureRegistry extends FeatureRegistry {
+object DefaultFeatureValueFeatureRegistry extends FeatureRegistry {
   override def feature(name: String): Option[Feature] = None
 
   override def allFeatures: Set[(FeatureName, Feature)] = Set.empty
